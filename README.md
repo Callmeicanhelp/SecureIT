941804d0e01aa03fa143e1ea4bc14c2e34419228
Callmeicanhelp <callmeicanhelp@gmail.com>
2022-08-25 01:40:45 -0400

    Updated README

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # password-generator
+
 Randomized password generation
 
-Created on VS Code using javascript 
+Created on VS Code using javascript
 
 This app is used to generate a random passowrd for our user when prompted for variables allowed to be used in the password, including upper and lower case letters, numbers, and some special characters.
 
 GitHub deployed page: https://callmeicanhelp.github.io/password-generator/
 
-Screenshot: assets/images/E4A90F10-43B2-4C63-B071-4F331FC459CF.jpeg
+Screenshot: assets/images/1FD3A1AD-FBB4-4066-A8D1-9EE78EFB9307.jpeg
--- script.js
+++ script.js
@@ -7,29 +7,29 @@ var specChars=["!#$%&'()*+,-./:;<=>?@[]^_`{|}~"];
 // Function to take user input and generate a password.
 function generatePassword() {
   //Prompt user for length of password.
-  var passLength = window.prompt("Choose a password length between 8 and 100 characters.");
+  var passLength = window.prompt("Choose a password length between 8 and 128 characters.");
 
-  //Verify that the user input is a number and length is between 8 and 100 characters.
-  while (!Number.isInteger(Number(passLength)) || passLength > 100 || passLength < 8) {
-    (passLength = window.prompt("Try again. Choose a password length between 8 and 100 characters."))
+  //Verify that the user input is a number and length is between 8 and 128 characters.
+  while (!Number.isInteger(Number(passLength)) || passLength > 128 || passLength < 8) {
+    (passLength = window.prompt("Try again. Choose a password length between 8 and 128 characters."))
   };
 
   //This variable will declare which characters are used.
   var usedChars = "";
 
-  //Confirms if user wants uppercase letters in the password and adds them to usesdChars if true.
+  //Confirms if user wants uppercase letters in the password and adds them to usedChars if true.
   var useUppercase = window.confirm("Will your password use uppercase letters?")
   if (useUppercase === true) { usedChars = usedChars + uppercase};
 
-  //Confirms if user wants lowercase letters in the password and adds them to usesdChars if true.
+  //Confirms if user wants lowercase letters in the password and adds them to usedChars if true.
   var useLowercase = window.confirm("Will your password use lowercase letters?")
   if (useLowercase === true) { usedChars = usedChars + lowercase};
 
-  //Confirms if user wants numbers in the password and adds them to usesdChars if true.
+  //Confirms if user wants numbers in the password and adds them to usedChars if true.
   var useNumbers = window.confirm("Will your password use numbers?")
   if (useNumbers === true) { usedChars = usedChars + numbers};
 
-  //Confirms if user wants special characters in the password and adds them to usesdChars if true.
+  //Confirms if user wants special characters in the password and adds them to usedChars if true.
   var useSpecChars = window.confirm("Will your password use special characters?")
   if (useSpecChars === true) { usedChars = usedChars + specChars};
 
